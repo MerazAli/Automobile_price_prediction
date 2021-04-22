dba38c59900dd80b2fe686472309a6033f675568
MerazAli <61820721+MerazAli@users.noreply.github.com>
2021-04-22 09:07:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 
 car=pd.read_csv('clean_car_dataset.csv')
 
+model = pickle.load(open('LinearRegressionModel.pkl','rb'))                 # load pickle model 
+
 @app.route('/')
 
 
@@ -20,6 +22,20 @@ def index():
     companies.insert(0,'Select company')
     
     return render_template('index.html', companies=companies,car_models=car_models,years=years,fuel_types=fuel_types)
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    company = request.form.get('company')
+    car_model = request.form.get('car_models')
+    year = request.form.get('year')
+    fuel_type = request.form.get('fuel_type')
+    driven = request.form.get('kilo_driven')
+
+    prediction = model.predict(pd.DataFrame(columns=['Name','company','year','kms_driven','fuel_type'], data=np.array([car_model,company,year,driven,fuel_type]).reshape(1,5)))
+
+    return str(np.round(prediction[0],3))
 
 
 
